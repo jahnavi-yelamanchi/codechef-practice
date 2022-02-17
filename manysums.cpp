c0d638331e9391c6bb916117c7efbfbbc718d188
Jahnavi Yelamanchi <jahnaviyelamanchi03@gmail.com>
2022-02-17 20:11:29 +0530

    added MANYSUMS c++ solution

---
+++ manysums.cpp
@@ -0,0 +1,16 @@
+//question => https://www.codechef.com/LP2TO301/problems/MANYSUMS
+
+#include <iostream>
+using namespace std;
+
+int main() {
+	int t;
+	cin>>t;
+	while(t--){
+	    int l,r;
+	    cin>>l>>r;
+	    int c = 2*r-2*l + 1;
+	    cout<<c<<endl;
+	}
+	return 0;
+}
